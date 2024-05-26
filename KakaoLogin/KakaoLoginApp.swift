aa809c6081723723e1fa6fa7681dacb5349c3eca
sikwan <yysskk99999@naver.com>
2024-05-26 20:19:20 +0900

    init

---
+++ KakaoLogin/KakaoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoLoginApp.swift
+//  KakaoLogin
+//
+//  Created by 양시관 on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct KakaoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
